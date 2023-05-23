8bb84cfa006b19e3c67a93d8c6c92454001eaf2f
David Ghedini <david@davidghedini.com>
2023-05-23 15:48:46 +0300

    Create database.config.php

---
+++ admin/class/database.config.php
@@ -0,0 +1,86 @@
+<?PHP
+		define('DB_HOST', '"db_host"');
+		define('DB_NAME', '"db_name"');
+		define('DB_SCMA', 'public');
+		define('DB_USER', '"db_username"');
+		define('DB_PASS', '"db_password"');
+		define('DB_PORT', 5432);
+
+    class Database {
+        private $connection;
+
+        function __construct($db_host, $db_name, $db_user, $db_pass, $db_port, $db_schema) {
+          $this->connection = pg_connect("dbname={$db_name} user={$db_user} password={$db_pass} host={$db_host} port={$db_port}");
+        }
+
+    	function modify($str) {
+        		return ucwords(str_replace("_", " ", $str));
+    	}
+
+				function getConn() {
+					return $this->connection;
+				}
+        function getAll($table, $where = '', $orderby = '') {
+            $orderby = $orderby ? 'ORDER BY '.$orderby : '';
+            $where = $where ? 'WHERE '.$where : '';
+
+            $query = "SELECT * FROM {$table} {$where} {$orderby}";
+            $result = pg_query($this->connection, $query);
+
+            if (!$result) {
+                echo "An error occurred executing the query.\n";
+                exit;
+            }
+
+            // Fetch all rows
+            $rows = array();
+            while ($row = pg_fetch_assoc($result)) {
+                $rows[] = $row;
+            }
+
+            return $rows;
+        }
+
+
+        function get($table, $where) {
+            if(is_numeric($where)) {
+                $where = "id = ".intval($where);
+            }
+            else if (empty($where)) {
+                $where = "1";
+            }
+
+            $query = "SELECT * FROM {$table} WHERE $where";
+            $result = pg_query($this->connection, $query);
+
+            if (!$result) {
+                echo "An error occurred executing the query.\n";
+                exit;
+            }
+
+            // Fetch one rows
+            $row = pg_fetch_assoc($result);
+
+            return $row;
+        }
+
+
+        /* Select Query */
+        function select($query) {
+            $result = pg_query($this->connection, $query);
+
+            if (!$result) {
+                echo "An error occurred executing the query.\n";
+                exit;
+            }
+
+            // Fetch all rows
+            $rows = array();
+            while ($row = pg_fetch_assoc($result)) {
+                $rows[] = $row;
+            }
+
+            return $rows;
+        }
+    }
+?>
